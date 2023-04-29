4275904bd9fbd87e2131977f6479d54793ebed58
Bhaskar Tripathi <35177508+bhaskatripathi@users.noreply.github.com>
2023-04-29 21:22:21 +0530

    Update vscode.js

--- components/apps/vscode.js
+++ components/apps/vscode.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function VsCode() {
     return (
-        <iframe src="https://github1s.com/bhaskatripathi/FusionGPT/blob/HEAD/pages/index.js" frameBorder="0" title="VsCode" className="h-full w-full bg-ub-cool-grey"></iframe>
+        <iframe src="https://github.dev/github/dev/bhaskatripathi/bhaskartripathi/blob/main/MY%20Machine%20Learning%20Experience.md" frameBorder="0" title="VsCode" className="h-full w-full bg-ub-cool-grey"></iframe>
     )
 }
 
