b2e4a7a85c95422fe54a4fc737d31465c94445b7
Bhaskar Tripathi <35177508+bhaskatripathi@users.noreply.github.com>
2023-05-04 01:30:46 +0530

    Update vscode.js

--- components/apps/vscode.js
+++ components/apps/vscode.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function VsCode() {
     return (
-        <iframe src="https://github1s.com/bhaskatripathi/bhaskartripathi/blob/main/My%20Experience%20Summary.md" frameBorder="0" title="VsCode" className="h-full w-full bg-ub-cool-grey"></iframe>
+        <iframe src="https://github1s.com/bhaskatripathi/bhaskartripathi/blob/main/Skillsets.jpeg" frameBorder="0" title="VsCode" className="h-full w-full bg-ub-cool-grey"></iframe>
     )
 }
 
