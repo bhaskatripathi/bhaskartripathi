e58802588a5d331d3362f65541ec37b6ca53ea7d
Bhaskar Tripathi <35177508+bhaskatripathi@users.noreply.github.com>
2024-11-29 07:23:09 +0530

    Update chrome.js

--- components/apps/chrome.js
+++ components/apps/chrome.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 export class Chrome extends Component {
     constructor() {
         super();
-        this.home_url = 'https://bhaskartripathi.com';
+        this.home_url = 'https://bhaskartripathi.substack.com/';
         this.state = {
-            url: 'https://bhaskartripathi.com',
-            display_url: "https://bhaskartripathi.com",
+            url: 'https://bhaskartripathi.substack.com/',
+            display_url: "https://bhaskartripathi.substack.com/",
         }
     }
 
